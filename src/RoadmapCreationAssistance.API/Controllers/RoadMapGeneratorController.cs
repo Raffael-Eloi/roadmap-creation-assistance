e30ec1590eeec1c6cd10d59deffc87fd51598158
Raffael Eloi <raffaeleloi121@gmail.com>
2026-01-25 20:04:52 -0300

    remove unnecessary example code

---
+++ src/RoadmapCreationAssistance.API/Controllers/RoadMapGeneratorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoadmapCreationAssistance.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoadMapGeneratorController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult GenerateRoadMap()
+    {
+        return Ok("success");
+    }
+}
