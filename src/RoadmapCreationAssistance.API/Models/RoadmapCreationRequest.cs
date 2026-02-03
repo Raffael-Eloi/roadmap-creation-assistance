67787c6932963ae03c5fc30a71c4debde785ccc3
Raffael Eloi <raffaeleloi121@gmail.com>
2026-02-03 11:12:30 -0300

    add validation to RoadmapCreationRequest

--- src/RoadmapCreationAssistance.API/Models/RoadmapCreationRequest.cs
+++ src/RoadmapCreationAssistance.API/Models/RoadmapCreationRequest.cs
@@ -4,12 +4,24 @@ namespace RoadmapCreationAssistance.API.Models;
 
 public sealed class RoadmapCreationRequest
 {
+    [Required(AllowEmptyStrings = false)]
+    [MinLength(5)]
+    [MaxLength(100)]
     public required string GitHubOwner { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [MinLength(5)]
+    [MaxLength(150)]
     public required string GitHubRepositoryName { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [MinLength(5)]
+    [MaxLength(60)]
     public required string GitHubToken { get; set; }
 
+    [Required(AllowEmptyStrings = false)]
+    [MinLength(5)]
+    [MaxLength(250)]
     public required string OpenAIKey { get; set; }
 
     [AllowedValues(["English-US", "Portuguese-BR"])]
