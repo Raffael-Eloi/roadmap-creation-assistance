a36acc7a737480e7612dfbedbc9d122ff734b3fd
Raffael Eloi <raffaeleloi121@gmail.com>
2026-02-25 22:46:31 -0300

    add registry URL to the ci/cd pipeline

--- src/RoadmapCreationAssistance.API/Program.cs
+++ src/RoadmapCreationAssistance.API/Program.cs
@@ -88,9 +88,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
